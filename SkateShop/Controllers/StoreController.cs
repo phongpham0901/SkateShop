eb9011ab3a0192bb5520e5a61f0cf2c534196b3d
Phạm Xuân Phong <phamxuanphong09012003@gmail.com>
2025-02-13 20:55:47 +0700

    Update 27

---
+++ SkateShop/Controllers/StoreController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkateShop.Models;
+using SkateShop.Services;
+
+namespace SkateShop.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly int pageSize = 8;
+
+        public StoreController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index(int pageIndex, string? search, string? name, string? brand, string? sort)
+        {
+            IQueryable<Product> query = context.Products;
+
+            //Tìm kiếm
+            if (search != null && search.Length > 0)
+            {
+                query = query.Where(p => p.Name.Contains(search));
+            }
+
+
+            // Lọc
+            if (brand != null && brand.Length > 0)
+            {
+                query = query.Where(p => p.Brand.Contains(brand));
+            }
+
+            
+
+            // Sắp xếp
+            if (sort == "price_asc")
+            {
+                query = query.OrderBy(p => p.Price);
+            }
+            else if (sort == "price_desc")
+            {
+                query = query.OrderByDescending(p => p.Price);
+            }
+            else if (sort == "oldest")
+            {
+                query = query.OrderBy(p => p.Id);
+            }
+            else
+            {
+                query = query.OrderByDescending(p => p.Id);
+            }
+
+
+            // phân trang
+            if (pageIndex < 1)
+            {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / pageSize);
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+
+            var products = query.ToList();
+
+            ViewBag.Products = products;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
+            var storeSearchModel = new StoreSearchModel()
+            {
+                Search = search,
+                Name = name,
+                Brand = brand,
+                Sort = sort
+            };
+
+            return View(storeSearchModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Store");
+            }
+
+            return View(product);
+        }
+    }
+}
---
+++ SkateShop/Models/StoreSearchModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SkateShop.Models
+{
+    public class StoreSearchModel
+    {
+        public string? Search { get; set; }
+        public string? Name { get; set; }
+        public string? Brand { get; set; }
+        public string? Sort { get; set; }
+    }
+}
